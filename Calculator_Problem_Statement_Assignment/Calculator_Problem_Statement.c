3c724dc699f307aa992f94d1bb365e22e4ef69bc
Vishal Menaria <vishalmenaria86@gmail.com>
2025-09-30 22:22:58 +0530

    added calculation function which checks all things like invalid operators divide error etc

--- Calculator_Problem_Statement_Assignment/Calculator_Problem_Statement.c
+++ Calculator_Problem_Statement_Assignment/Calculator_Problem_Statement.c
@@ -2,6 +2,81 @@
 
 #define size 100
 
+int calculation(char *expr)
+{
+    int lastOpIdx = -1;
+    int res = 0;
+    int term = 0;
+    char prevOp = '+';
+    int i = 0;
+
+    while (expr[i] != '\0')
+    {
+        if (expr[i] >= '0' && expr[i] <= '9')
+        {
+            int operand = 0;
+            while (expr[i] >= '0' && expr[i] <= '9')
+            {
+                operand = operand * 10 + (expr[i] - '0');
+                i++;
+            }
+
+            if (prevOp == '+')
+            {
+                term = operand;
+            }
+            else if (prevOp == '-')
+            {
+                term = -(operand);
+            }
+            else if (prevOp == '*')
+            {
+                term = term * operand;
+            }
+            else if (prevOp == '/')
+            {
+                if (operand == 0)
+                {
+                    printf("Error: Division by zero.\n");
+                    return -1;
+                }
+                term = term / operand;
+            }
+        }
+        else if (expr[i] == '+' || expr[i] == '-')
+        {
+            // printf("%d %d\n",lastOpIdx, i);
+            if (lastOpIdx == i - 1)
+            {
+                printf("Error: Invalid expression.\n");
+                return -1;
+            }
+            lastOpIdx = i;
+            res += term;
+            prevOp = expr[i];
+            i++;
+        }
+        else if (expr[i] == '*' || expr[i] == '/')
+        {
+            if (lastOpIdx == i - 1)
+            {
+                printf("Error: Invalid expression.\n");
+                return -1;
+            }
+            lastOpIdx = i;
+            prevOp = expr[i];
+            i++;
+        }
+        else
+        {
+            printf("Error: Invalid expression.\n");
+            return -1;
+        }
+    }
+    res += term;
+    return res;
+}
+
 int main()
 {
     char expression[size];
@@ -27,6 +102,13 @@ int main()
     {
         printf("invalid string\n");
         return 0;
+    }
+
+    int result = calculation(expression);
+
+    if (result != -1)
+    {
+        printf("%d\n", result);
     }
 
     return 0;
